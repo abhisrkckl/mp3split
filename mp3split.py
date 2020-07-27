d770b4ca1d47911a4657ae5cb40887b52fcf4805
Abhimanyu S <abhisrkckl@gmail.com>
2020-07-27 13:23:23 +0530

    no exec option

--- mp3split.py
+++ mp3split.py
@@ -5,7 +5,7 @@ import sys
 import numpy as np
 import getopt
 
-opts, args = getopt.getopt(sys.argv[1:], 'ha:l:y:')
+opts, args = getopt.getopt(sys.argv[1:], 'hXa:l:y:')
 
 helpmsg = "mp3split.py <mp3_file> <split_info_file>"
 if len(args)<2:
@@ -15,16 +15,17 @@ if len(args)<2:
 bigmp3 = args[0]
 infofile = args[1]
 
+noexec = False
+
 id3_opts = ""
 for o,a in opts:
     if o=='-h':
         print(helpmsg)
         sys.exit(0)
+    elif o=='-X':
+        noexec = True
     else:
         id3_opts += " {} \"{}\"".format(o,a)
-
-print(id3_opts)
-
 
 info = np.genfromtxt(infofile, dtype=str, delimiter=',')
 
@@ -37,10 +38,12 @@ for idx, (start, songname) in enumerate(info):
         stop = info[idx+1][0]
         cmd = "ffmpeg -i \"{}\" -acodec copy -ss {} -to {} \"{}.mp3\" ".format(bigmp3, start, stop, songname)
     print(cmd)
-    os.system(cmd)
+    if not noexec:
+        os.system(cmd)
 
     if len(id3_opts)>0:
         id3_cmd = "mp3info " + id3_opts + " -n {} -t \"{}\" ".format(idx+1, songname) + smallmp3
         print(id3_cmd)
-        os.system(id3_cmd)
+        if not noexec:
+            os.system(id3_cmd)
 
