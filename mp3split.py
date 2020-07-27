cfd17eb47fc68e833b6a11700076ec86ee06f6a6
Abhimanyu S <abhisrkckl@gmail.com>
2020-07-27 13:15:59 +0530

    mp3 Tagging with mp3info

--- mp3split.py
+++ mp3split.py
@@ -3,22 +3,44 @@
 import os
 import sys
 import numpy as np
+import getopt
 
-if len(sys.argv)<3:
-    print("mp3split.py <mp3_file> <split_info_file>")
+opts, args = getopt.getopt(sys.argv[1:], 'ha:l:y:')
+
+helpmsg = "mp3split.py <mp3_file> <split_info_file>"
+if len(args)<2:
+    print(helpmsg)
     sys.exit(0)
 
-bigmp3 = sys.argv[1]
-infofile = sys.argv[2]
+bigmp3 = args[0]
+infofile = args[1]
 
-info = np.genfromtxt(infofile, dtype=str)
+id3_opts = ""
+for o,a in opts:
+    if o=='-h':
+        print(helpmsg)
+        sys.exit(0)
+    else:
+        id3_opts += " {} \"{}\"".format(o,a)
+
+print(id3_opts)
+
+
+info = np.genfromtxt(infofile, dtype=str, delimiter=',')
+
 
 for idx, (start, songname) in enumerate(info):
+    smallmp3 = "\"{}.mp3\"".format(songname)
     if idx==len(info)-1:
-        cmd = "ffmpeg -i \"{}\" -acodec copy -ss {} \"{}.mp3\"".format(bigmp3, start, songname)
+        cmd = "ffmpeg -i \"{}\" -acodec copy -ss {} {} ".format(bigmp3, start, smallmp3)
     else:
         stop = info[idx+1][0]
-        cmd = "ffmpeg -i \"{}\" -acodec copy -ss {} -to {} \"{}.mp3\"".format(bigmp3, start, stop, songname)
+        cmd = "ffmpeg -i \"{}\" -acodec copy -ss {} -to {} \"{}.mp3\" ".format(bigmp3, start, stop, songname)
     print(cmd)
     os.system(cmd)
+
+    if len(id3_opts)>0:
+        id3_cmd = "mp3info " + id3_opts + " -n {} ".format(idx+1) + smallmp3
+        print(id3_cmd)
+        os.system(id3_cmd)
 
